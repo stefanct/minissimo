msfschaffner/common_cells

onehot_to_bin: Add onehot assertion

src/onehot_to_bin.sv
@@ -12,9 +12,10 @@
 
 module onehot_to_bin #(
     parameter int unsigned ONEHOT_WIDTH = 16,
-    parameter int unsigned BIN_WIDTH    = $clog2(ONEHOT_WIDTH)
+    // Do Not Change
+    localparam int unsigned BIN_WIDTH   = $clog2(ONEHOT_WIDTH)
 )(
-    input logic  [ONEHOT_WIDTH-1:0] onehot,
+    input  logic [ONEHOT_WIDTH-1:0] onehot,
     output logic [BIN_WIDTH-1:0]    bin
 );
 
@@ -27,4 +28,12 @@ module onehot_to_bin #(
             end
         assign bin[j] = |(tmp_mask & onehot);
     end
+
+// pragma translate_off
+`ifndef VERILATOR
+    initial begin
+        assert($onehot0(onehot)) else $fatal(1, "[onehot_to_bin] More than two bit set in the one-hot signal");
+    end
+`endif
+// pragma translate_on
 endmodule

stefanct/minissimo

ips/fpnew/src/common_cells/src/onehot_to_bin.sv
@@ -12,9 +12,10 @@
 
 module onehot_to_bin #(
     parameter int unsigned ONEHOT_WIDTH = 16,
-    parameter int unsigned BIN_WIDTH    = $clog2(ONEHOT_WIDTH)
+    // Do Not Change
+    parameter int unsigned BIN_WIDTH   = $clog2(ONEHOT_WIDTH)
 )(
-    input logic  [ONEHOT_WIDTH-1:0] onehot,
+    input  logic [ONEHOT_WIDTH-1:0] onehot,
     output logic [BIN_WIDTH-1:0]    bin
 );
 
@@ -27,4 +28,12 @@ module onehot_to_bin #(
             end
         assign bin[j] = |(tmp_mask & onehot);
     end
+
+// pragma translate_off
+`ifndef VERILATOR
+    initial begin
+        assert($onehot0(onehot)) else $fatal(1, "[onehot_to_bin] More than two bit set in the one-hot signal");
+    end
+`endif
+// pragma translate_on
 endmodule