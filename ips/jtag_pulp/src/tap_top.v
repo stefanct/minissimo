pulp-platform/jtag_pulp

port to 5bits instructions

src/tap_top.v
@@ -26,17 +26,17 @@
 // 1                required by standard
 
 // Length of the Instruction register
-`define	IR_LENGTH	4
+`define	IR_LENGTH	5
 
 // Supported Instructions
-`define IDCODE          4'b0010
-`define REG1            4'b0100
-`define REG2            4'b0101
-`define REG3            4'b0110
-`define REG4            4'b0111
-`define REG5            4'b1000
-`define REG6            4'b1001
-`define BYPASS          4'b1111
+`define IDCODE          5'b00010
+`define REG1            5'b00100
+`define REG2            5'b00101
+`define REG3            5'b00110
+`define REG4            5'b00111
+`define REG5            5'b01000
+`define REG6            5'b01001
+`define BYPASS          5'b11111
 
 // Top module
 module tap_top(
@@ -398,7 +398,7 @@ begin
   if(~rst_ni)
     jtag_ir[`IR_LENGTH-1:0] <=  `IR_LENGTH'b0;
   else if(capture_ir)
-    jtag_ir <=  4'b0101;          // This value is fixed for easier fault detection
+    jtag_ir <=  5'b00101;          // This value is fixed for easier fault detection
   else if(shift_ir)
     jtag_ir[`IR_LENGTH-1:0] <=  {td_i, jtag_ir[`IR_LENGTH-1:1]};
 end

stefanct/minissimo

ips/jtag_pulp/src/tap_top.v
@@ -26,17 +26,17 @@
 // 1                required by standard
 
 // Length of the Instruction register
-`define	IR_LENGTH	4
+`define	IR_LENGTH	5
 
 // Supported Instructions
-`define IDCODE          4'b0010
-`define REG1            4'b0100
-`define REG2            4'b0101
-`define REG3            4'b0110
-`define REG_CLK_BYP     4'b0111
-`define REG_OBSERV      4'b1000
-`define REG6            4'b1001
-`define BYPASS          4'b1111
+`define IDCODE          5'b00010
+`define REG1            5'b00100
+`define REG2            5'b00101
+`define REG3            5'b00110
+`define REG_CLK_BYP     5'b00111
+`define REG_OBSERV      5'b01000
+`define REG6            5'b01001
+`define BYPASS          5'b11111
 
 // Top module
 module tap_top(
@@ -413,7 +413,7 @@ begin
   if(~rst_ni)
     jtag_ir[`IR_LENGTH-1:0] <=  `IR_LENGTH'b0;
   else if(capture_ir)
-    jtag_ir <=  4'b0101;          // This value is fixed for easier fault detection
+    jtag_ir <=  5'b00101;          // This value is fixed for easier fault detection
   else if(shift_ir)
     jtag_ir[`IR_LENGTH-1:0] <=  {td_i, jtag_ir[`IR_LENGTH-1:1]};
 end